9fc5e4207deddc49fa93a41e450b8ab2db5eb61e
timiil <6621768+timiil@users.noreply.github.com>
2017-10-17 23:36:15 +0800

    Delete patch.sh

--- patch.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cp 4.4_qcom-ipq8065-r7800.dts /lede/target/linux/ipq806x/files-4.4/arch/arm/boot/dts/qcom-ipq8065-r7800.dts
-cp 4.9_qcom-ipq8065-r7800.dts /lede/target/linux/ipq806x/files-4.9/arch/arm/boot/dts/qcom-ipq8065-r7800.dts
-cp .config /lede/.config
